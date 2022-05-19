36ce29f19ec550e9f3122311cf916158a9d4d4a0
Molly <Molly@DESKTOP-0MJFMRI>
2022-05-19 14:22:28 -0400

    added social links to footer, changed email in footer

--- php/footer.php
+++ php/footer.php
@@ -1,8 +1,13 @@
 <?php
 echo " <footer class=\"footer\">\n
 <p>Molly Viau ©</p>\n
-<p>Email me at <a class=\"emailText\" href=\"mailto:&#118;&#105;&#97;&#117;&#109;&#111;&#108;&#108;&#121;&#64;&#103;&#109;&#97;&#105;&#108;&#46;&#99;&#111;&#109;\">
-<p class=\"emailText\">&#118;&#105;&#97;&#117;&#109;&#111;&#108;&#108;&#121;&#64;&#103;&#109;&#97;&#105;&#108;&#46;&#99;&#111;&#109;</p></a>
+
+
+<p>Email me at <a class=\"emailText\" href=\"mailto:contact@mollyviau.com\">
+<p class=\"emailText\">contact@mollyviau.com</p></a>
 <!-- <button id=\"copyButton\" type=\"button\" onclick=\"copyEvent(\'emailText\')\">Copy</button></p>\n --> 
+<p><a href=\"https://www.linkedin.com/in/mollyviau/\" target=\"_blank\"><i class=\"fa-brands fa-linkedin\"></i></a></p>
+<p><a href=\"https://github.com/mollyviau/\" target=\"_blank\"><i class=\"fa-brands fa-github-square\"></i></a></p>
+
 </footer>";
 ?>
--- php/head.php
+++ php/head.php
@@ -1,17 +1,20 @@
 <?php
 echo "  <head>\n
         <meta charset=\"utf-8\">\n        
-        <meta http-equiv=\"X-UA-Compatible\" content=\"IE=edge\">\n        
+        <meta http-equiv=\"X-UA-Compatible\" content=\"IE=edge\">\n
+        <meta name=\"description\" content=\"Web Developer Remote Canada Ontario Design Website\" />      
+        <meta name=\"viewport\" content=\"width=device-width, initial-scale=1\">\n
+
         <title>Remote Web Developer Canada</title>\n        
-        <meta name=\"description\" content=\"\">\n        
-        <meta name=\"viewport\" content=\"width=device-width, initial-scale=1\">\n        
+
         <link rel=\"stylesheet\" href=\"css\\style.css\">\n        
         <link rel=\"preconnect\" href=\"https://fonts.googleapis.com\">\n        
         <link rel=\"preconnect\" href=\"https://fonts.gstatic.com\" crossorigin>\n        
         <link href=\"https://fonts.googleapis.com/css2?family=Open+Sans:wght@300&display=swap\" rel=\"stylesheet\">\n
 
+        <script src=\"https://kit.fontawesome.com/8dbfcb90e4.js\" crossorigin=\"anonymous\"></script>
+        
         <link rel=\"icon\" type=\"image/x-icon\" href=\"favicon.ico\">
-
         <link rel=\"apple-touch-icon\" sizes=\"76x76\" href=\"../apple-touch-icon.png\">
         <link rel=\"icon\" type=\"image/png\" sizes=\"32x32\" href=\"../favicon-32x32.png\">
         <link rel=\"icon\" type=\"image/png\" sizes=\"16x16\" href=\"../favicon-16x16.png\">
