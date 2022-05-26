e6233aa608ea96bbe9d6b48a1fbdd3cdc8945ca4
Molly <Molly@DESKTOP-0MJFMRI>
2022-05-26 15:17:39 -0400

    fixing duplicate link in footer

--- php/footer.php
+++ php/footer.php
@@ -5,7 +5,6 @@ echo " <footer class=\"footer\">\n
 
 <p>Email me at <a class=\"emailText\" href=\"mailto:contact@mollyviau.com\">
 <p class=\"emailText\">contact@mollyviau.com</p></a>
-<!-- <button id=\"copyButton\" type=\"button\" onclick=\"copyEvent(\'emailText\')\">Copy</button></p>\n --> 
 <p><a href=\"https://www.linkedin.com/in/mollyviau/\" target=\"_blank\"><i class=\"fa-brands fa-linkedin\"></i></a></p>
 <p><a href=\"https://github.com/mollyviau/\" target=\"_blank\"><i class=\"fa-brands fa-github-square\"></i></a></p>
 
