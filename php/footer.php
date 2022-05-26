f6473a935e9215bffd41e25078ca3a769e914a3a
Molly <Molly@DESKTOP-0MJFMRI>
2022-05-26 15:32:34 -0400

    trying to fix redundant link message from wave for email link footer

--- php/footer.php
+++ php/footer.php
@@ -4,7 +4,7 @@ echo " <footer class=\"footer\">\n
 
 
 <p>Email me at <a aria-label=\"click here to email Molly\" class=\"emailText\" href=\"mailto:contact@mollyviau.com\">
-<p class=\"emailText\">contact@mollyviau.com</p></a>
+<!-- <p class=\"emailText\">contact@mollyviau.com</p></a> -->
 <p><a aria-label=\"click here to open Molly's LinkedIn\" href=\"https://www.linkedin.com/in/mollyviau/\" target=\"_blank\"><i tabindex=\"0\"  alt=\"click here to open Molly's LinkedIn\" title=\"click here to open Molly's LinkedIn\" class=\"fa-brands fa-linkedin\"></i></a></p>
 <p><a aria-label=\"click here to open Molly's GitHub\" href=\"https://github.com/mollyviau/\" target=\"_blank\"><i tabindex=\"0\"   alt=\"click here to open Molly's GitHub\" title=\"click here to open Molly's GitHub\" class=\"fa-brands fa-github-square\"></i></a></p>
 
