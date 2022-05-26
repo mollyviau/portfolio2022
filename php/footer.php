428736a49b150c9306a76387a0916dce380fe0e4
Molly <Molly@DESKTOP-0MJFMRI>
2022-05-26 15:35:52 -0400

    fixing email link footer

--- php/footer.php
+++ php/footer.php
@@ -3,7 +3,7 @@ echo " <footer class=\"footer\">\n
 <p>Molly Viau ©</p>\n
 
 
-<p>Email me <a aria-label=\"click here to email Molly\" class=\"emailText\" href=\"mailto:contact@mollyviau.com\"> here </a></p>
+<p>Email me <a title=\"contact@mollyviau.com\" aria-label=\"click here to email Molly\" class=\"emailText\" href=\"mailto:contact@mollyviau.com\"> here </a></p>
 <!-- <p class=\"emailText\">contact@mollyviau.com</p></a> -->
 <p><a aria-label=\"click here to open Molly's LinkedIn\" href=\"https://www.linkedin.com/in/mollyviau/\" target=\"_blank\"><i tabindex=\"0\"  alt=\"click here to open Molly's LinkedIn\" title=\"click here to open Molly's LinkedIn\" class=\"fa-brands fa-linkedin\"></i></a></p>
 <p><a aria-label=\"click here to open Molly's GitHub\" href=\"https://github.com/mollyviau/\" target=\"_blank\"><i tabindex=\"0\"   alt=\"click here to open Molly's GitHub\" title=\"click here to open Molly's GitHub\" class=\"fa-brands fa-github-square\"></i></a></p>
