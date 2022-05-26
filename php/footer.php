09c0c1d070d0dacabd8249c204281f0384011864
Molly <Molly@DESKTOP-0MJFMRI>
2022-05-26 15:24:10 -0400

    added title and tabindex to footer social media icons

--- php/footer.php
+++ php/footer.php
@@ -5,8 +5,8 @@ echo " <footer class=\"footer\">\n
 
 <p>Email me at <a class=\"emailText\" href=\"mailto:contact@mollyviau.com\">
 <p class=\"emailText\">contact@mollyviau.com</p></a>
-<p><a href=\"https://www.linkedin.com/in/mollyviau/\" target=\"_blank\"><i alt =\"click here to open Molly's LinkedIn\" class=\"fa-brands fa-linkedin\"></i></a></p>
-<p><a href=\"https://github.com/mollyviau/\" target=\"_blank\"><i alt =\"click here to open Molly's GitHub\" class=\"fa-brands fa-github-square\"></i></a></p>
+<p><a href=\"https://www.linkedin.com/in/mollyviau/\" target=\"_blank\"><i tabindex=\"0\" alt=\"click here to open Molly's LinkedIn\" title=\"click here to open Molly's LinkedIn\" class=\"fa-brands fa-linkedin\"></i></a></p>
+<p><a href=\"https://github.com/mollyviau/\" target=\"_blank\"><i tabindex=\"0\" alt=\"click here to open Molly's GitHub\" title=\"click here to open Molly's GitHub\" class=\"fa-brands fa-github-square\"></i></a></p>
 
 </footer>";
 ?>
